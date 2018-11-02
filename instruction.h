afca3b46caef9d29ed68d88b027a83a5cedc228a
david <carew124@gmail.com>
2018-11-01 22:21:59 -0700

    Use a real enum for instruction opcodes

--- instruction.h
+++ instruction.h
@@ -1,7 +1,25 @@
-struct {
+enum opcodes{
+      exit = 0x00,
+      push = 0x01,
+      add = 0x02,
+      sub = 0x03,
+      mul = 0x04,
+      div = 0x05,
+      cmp = 0x06,
+      ld = 0x07,
+      sto = 0x08,
+      io = 0x09,
+      dup = 0x0A,
+      swp = 0x0B,
+      drp = 0x0C,
+      jmp = 0x0D,
+      call = 0x0E,
+      ret = 0x0F
+};
+struct instruction_{
   struct token start;
-  int opcode;
-  int args[3];
-} instruction_;
 
-typedef struct instruction_ *instruction
+  int args[3];
+};
+
+typedef struct instruction_ *instruction;
