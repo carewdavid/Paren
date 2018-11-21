38e597e9f2e72879837ce3b7565a7dfb710064cb
david <carew124@gmail.com>
2018-11-20 21:47:22 -0800

    Uppercase opcode names so they don't conflict with identifiers from other files

--- instruction.h
+++ instruction.h
@@ -1,25 +1,24 @@
 enum opcodes{
-      exit = 0x00,
-      push = 0x01,
-      add = 0x02,
-      sub = 0x03,
-      mul = 0x04,
-      div = 0x05,
-      cmp = 0x06,
-      ld = 0x07,
-      sto = 0x08,
-      io = 0x09,
-      dup = 0x0A,
-      swp = 0x0B,
-      drp = 0x0C,
-      jmp = 0x0D,
-      call = 0x0E,
-      ret = 0x0F
+      PUSH = 0x00,
+      ADD = 0x01,
+      SUB = 0x02,
+      MUL = 0x03,
+      DIV = 0x04,
+      CMP = 0x05,
+      LD = 0x06,
+      STO = 0x07,
+      IO = 0x08,
+      DUP = 0x09,
+      SWP = 0x0A,
+      DRP = 0x0B,
+      JMP = 0x0C,
+      CALL = 0x0D,
+      RET = 0x0E,
+      EXIT = 0x0F,
 };
-struct instruction_{
+struct instruction{
   struct token start;
-
+  enum opcodes opcode;
   int args[3];
 };
 
-typedef struct instruction_ *instruction;
