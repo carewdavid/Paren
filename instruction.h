bf4c8d041ec990aae5f219d73b00884581cdf36a
david <carew124@gmail.com>
2019-02-15 23:58:27 -0800

    Clean up instruction header

--- instruction.h
+++ instruction.h
@@ -1,25 +1,18 @@
-enum opcodes{
-      exit = 0x00,
-      push = 0x01,
-      add = 0x02,
-      sub = 0x03,
-      mul = 0x04,
-      div = 0x05,
-      cmp = 0x06,
-      ld = 0x07,
-      sto = 0x08,
-      io = 0x09,
-      dup = 0x0A,
-      swp = 0x0B,
-      drp = 0x0C,
-      jmp = 0x0D,
-      call = 0x0E,
-      ret = 0x0F
-};
-struct instruction_{
-  struct token start;
-
-  int args[3];
-};
-
-typedef struct instruction_ *instruction;
+typedef enum {
+      OP_PUSH,
+      OP_ADD,
+      OP_SUB,
+      OP_MUL,
+      OP_DIV,
+      OP_CMP,
+      OP_LOAD,
+      OP_STORE,
+      OP_READ,
+      OP_WRITE,
+      OP_DUP,
+      OP_SWAP,
+      OP_DROP,
+      OP_GOTO,
+      OP_CALL,
+      OP_EXIT
+} OpCode;
